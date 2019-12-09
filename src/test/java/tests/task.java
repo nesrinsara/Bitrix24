80cfa8850d230cede741740693e854114a1f5633
nesrinsara <57195526+nesrinsara@users.noreply.github.com>
2019-12-09 18:44:28 +0100

    Update task.java

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,4 +1,9 @@
 package tests;
 
 public class task {
+public static void main(String[]args){
+  
+  System.out.println("Hello World");
+  //this task updated from GitHub, see you in intelliJ
+}  
 }
