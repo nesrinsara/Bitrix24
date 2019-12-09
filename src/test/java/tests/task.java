34c6e4134458232bef6916c0d3be5d88ca0ea505
nesrinsara <57195526+nesrinsara@users.noreply.github.com>
2019-12-09 19:18:27 +0100

    added return method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -5,5 +5,9 @@ public static void main(String[]args){
   
   System.out.println("Hello World");
   //this task updated from GitHub, see you in intelliJ
-}  
+  
+} 
+public String hello(){
+ return "Hello from GitHub"; 
+}
 }
