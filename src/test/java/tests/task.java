2577d05dcb5e7069751c800743bcb26ef011d7b9
Nesrin <nesrinsara@hotmail.com>
2019-12-09 19:15:36 +0100

    add void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,9 +1,13 @@
 package tests;
 
 public class task {
-public static void main(String[]args){
-  
-  System.out.println("Hello World");
-  //this task updated from GitHub, see you in intelliJ
-}  
+public static void main(String[]args) {
+
+    System.out.println("Hello World");
+    //this task updated from GitHub, see you in intelliJ
+
+}
+public void hi(){
+    System.out.println("Hi from IntelliJ");
+}
 }
